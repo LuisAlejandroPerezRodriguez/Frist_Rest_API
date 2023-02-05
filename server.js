20688122a7ec9b2b75b6865b7444303a34a10c09
Luis Alejandro Pérez Rodríguez <luisalejandro.p.r@outlook.com>
2023-02-05 14:13:29 -0400

    Se agrego la funcion de get, post y get con ID

--- server.js
+++ server.js
@@ -2,15 +2,25 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
+const products = [ //Para poder guardar los productos tengo que hacer uso de un archivo o una base de datos
+    {
+        id: 1,
+        name: "Monitor",
+        price: 300
+    }
+]
 
 app.use(morgan('dev'))
+app.use(express.json())
 
 app.get('/product', (req,res) => {
-    res.send('Obteniendo productos')
+    res.json(products)
 })
 
 app.post('/product', (req,res) => {
-    res.send('creando productos')
+    const newProduct = {... req.body, id: products.length + 1}
+    products.push(newProduct)
+    res.send(newProduct)
 
 })
 
@@ -25,8 +35,15 @@ app.delete('/product', (req,res) => {
 })
 
 app.get('/product/:id', (req,res) => {
-    res.send('obteniendo un productos')
+    const productFound = products.find(function (product){
+        return product.id == req.params.id
+    })
 
+    if (!productFound)  return res.status(404).json({
+        message: " No se encuentra el producto"
+    })
+
+    res.json(productFound)
 })
 
 app.listen(3000)
