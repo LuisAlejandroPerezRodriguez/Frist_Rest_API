81b3f807a3349b8e3c8e49b7a26806ccbc92b850
Luis Alejandro Pérez Rodríguez <luisalejandro.p.r@outlook.com>
2023-02-05 13:12:40 -0400

    Se crearon las peticiones basicas del client

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
-console.log("Hola mundo")
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'))
+
+app.get('/product', (req,res) => {
+    res.send('Obteniendo productos')
+})
+
+app.post('/product', (req,res) => {
+    res.send('creando productos')
+
+})
+
+app.put('/product', (req,res) => {
+    res.send('actualizando productos')
+
+})
+
+app.delete('/product', (req,res) => {
+    res.send('eliminando productos')
+
+})
+
+app.get('/product/:id', (req,res) => {
+    res.send('obteniendo un productos')
+
+})
+
+app.listen(3000)
+console.log(`server on port ${3000}`)
